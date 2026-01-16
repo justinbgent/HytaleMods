b356e650f0fa864e949867c4957f66908fcc6225
Justin Gent <justinbgent@gmail.com>
2026-01-15 21:35:53 -0700

    Initial commit using template from https://github.com/realBritakee/hytale-template-plugin?tab=readme-ov-file

---
+++ src/main/java/com/edgelinegames/cameraplugin/CameraPlugin.java
@@ -0,0 +1,55 @@
+package com.edgelinegames.cameraplugin;
+
+/**
+ * Main plugin class.
+ * 
+ * Made with template from:
+ * https://github.com/realBritakee/hytale-template-plugin?tab=readme-ov-file
+ * 
+ * @author JustinGent
+ * @version 1.0.0
+ */
+public class CameraPlugin {
+
+    private static CameraPlugin instance;
+    
+    /**
+     * Constructor - Called when plugin is loaded.
+     */
+    public CameraPlugin() {
+        instance = this;
+        System.out.println("[TemplatePlugin] Plugin loaded!");
+    }
+    
+    /**
+     * Called when plugin is enabled.
+     */
+    public void onEnable() {
+        System.out.println("[TemplatePlugin] Plugin enabled!");
+        
+        // TODO: Initialize your plugin here
+        // - Load configuration
+        // - Register event listeners
+        // - Register commands
+        // - Start services
+    }
+    
+    /**
+     * Called when plugin is disabled.
+     */
+    public void onDisable() {
+        System.out.println("[TemplatePlugin] Plugin disabled!");
+        
+        // TODO: Cleanup your plugin here
+        // - Save data
+        // - Stop services
+        // - Close connections
+    }
+    
+    /**
+     * Get plugin instance.
+     */
+    public static CameraPlugin getInstance() {
+        return instance;
+    }
+}
